0a8baa16fbf8b37a57404cd29a0bb41c03ad11fd
Nikita <gavrilovnikita2004@gmail.com>
2025-08-02 00:58:05 +0300

    added-dev-server-to-webpack

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { calc } from "./test";
 
-console.log("hello world");
+console.log("hello world from dev server!");
 console.log(calc(5, 5));
---
+++ webpack.config.ts
@@ -0,0 +1,40 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+
+type Mode = "production" | "development";
+
+interface EnvVariables {
+  mode: Mode;
+}
+
+export default (env: EnvVariables) => {
+  const config: webpack.Configuration = {
+    mode: env.mode ?? "development",
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].js",
+      clean: true,
+    },
+    plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, "public", "index.html") })],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    devServer: {
+      port: 5000,
+      open: true,
+    },
+  };
+  return config;
+};
